cf0807ea84ccf9511d0661d822b1f6455f401fc8
Irfaneva99 <109207022+Irfaneva99@users.noreply.github.com>
2022-11-14 08:37:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','6285731811079','6285339388512']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6289510497533','6289510497533','6289510497533']
+global.premium = ['6289510497533']
+global.packname = 'Ipan Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
